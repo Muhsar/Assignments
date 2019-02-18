cbc99c4e06fdacb04f216e33cbbd6f9cf91593c4
Jubril Musa <Jewbreel1@gmail.com>
2019-02-18 03:44:16 -0800

    created new branch

---
+++ script.js
@@ -0,0 +1,30 @@
+function myFunction1(){
+var box = document.getElementById('box')
+    
+    box.style.backgroundColor = 'red'
+}
+myFunction1()
+function myFunction2(){
+    var box = document.getElementById('box')
+        
+        box.style.backgroundColor = 'yellow'
+    }
+myFunction2()
+function myFunction3(){
+    var box = document.getElementById('box')
+        
+        box.style.backgroundColor = 'blue'
+    }
+myFunction3()
+function myFunction4(){
+    var box = document.getElementById('box')
+        
+        box.style.backgroundColor = 'green'
+    }
+myFunction4()
+function myFunction5(){
+    var box = document.getElementById('box')
+        
+        box.style.backgroundColor = 'purple'
+    }
+myFunction5()
